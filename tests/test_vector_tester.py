d21656f44e4f98d4dc20f12ad099c102fa07c437
nyuta <nyuta.main@gmail.com>
2022-06-27 08:59:44 +0000

    create sample of get price data

--- tests/test_vector_tester.py
+++ tests/test_vector_tester.py
@@ -1,24 +1,26 @@
-from backtester.vector_tester import VectorTester
-import pandas as pd
-import numpy as np
 import warnings
-warnings.simplefilter('ignore')
 
-def test_run_back_test()->None:
+import numpy as np
+import pandas as pd
+from backtester.vector_tester import VectorTester
+
+warnings.simplefilter("ignore")
+
+
+def test_run_back_test() -> None:
     # CSVの読み込み
     raw = pd.read_csv("./backtester/data/bybit/2022_1_BTC.csv").dropna()
     # 終値のみ切り抜いて新たなデータフレームに格納
-    data = raw[['close','timestamp']]
+    data = raw[["close", "timestamp"]]
     # 移動平均線の格納
-    data['SMA1'] = data['close'].rolling(10).mean()
-    data['SMA2'] = data['close'].rolling(50).mean()
+    data["SMA1"] = data["close"].rolling(10).mean()
+    data["SMA2"] = data["close"].rolling(50).mean()
     # シグナルを作る
-    data['b_sig'] = np.where(data['SMA1'] > data['SMA2'],1,0)
-    data['s_sig'] = np.where(data['SMA2'] > data['SMA1'],1,0)
+    data["b_sig"] = np.where(data["SMA1"] > data["SMA2"], 1, 0)
+    data["s_sig"] = np.where(data["SMA2"] > data["SMA1"], 1, 0)
     data = data.dropna()
 
     tester = VectorTester("BTC")
     result = tester.run_backtest(df=data)
     tester.make_pl(result)
     assert type(tester.make_pl(result)) == pd.DataFrame
-
